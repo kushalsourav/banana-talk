1323b1dcbf0bb9912b5722e33ca1e06d5115bff9
kushalsourav <kushalsourav2@gmail.com>
2021-12-04 15:24:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,10 @@ var btnTranslator = document.querySelector('#btn-translator');
 var textInput = document.querySelector('#textinput')
 var output = document.querySelector('#output');
 
-var serverUrl = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+// var serverUrl = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+
+var serverUrl = "https://api.funtranslations.com/translate/minion.json";
+
 
 function getTranslator(input) {
     return serverUrl + "?" + "text" + input
