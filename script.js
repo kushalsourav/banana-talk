d480b1027ffefa50732ad195d2af0bf112515a7c
kushalsourav <kushalsourav2@gmail.com>
2021-12-04 15:16:14 +0530

    done

---
+++ script.js
@@ -0,0 +1,30 @@
+console.log("hello")
+
+var btnTranslator = document.querySelector('#btn-translator');
+var textInput = document.querySelector('#textinput')
+var output = document.querySelector('#output');
+
+var serverUrl = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+
+function getTranslator(input) {
+    return serverUrl + "?" + "text" + input
+}
+
+function errorHandler(err) {
+    console.log("Error Occured :", err);
+}
+
+function clickHandler() {
+    var inputText = textInput.value;
+    fetch(getTranslator(inputText))
+        .then(response => response.json())
+        .then(json => {
+            var translatedText = json.contents.translated;
+            output.innerText = translatedText;
+        })
+        .catch(errorHandler);
+
+}
+
+
+btnTranslator.addEventListener('click', clickHandler)
